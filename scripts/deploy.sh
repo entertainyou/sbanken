759776240a2fb604f8237b8ed49a5255ae0cf597
Wei Huang <grubbyfans@gmail.com>
2019-02-24 21:53:21 +0100

    Correct deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 KEY_FILE=/tmp/travis_rsa
-BUILD=_build/prod/rel/sbanken_monitor/releases/*/sbanken_monitor.tar.gz
-DEST_FOLDER=sbanken_monitor
+BUILD=_build/prod/rel/sbanken/releases/*/sbanken.tar.gz
+DEST_FOLDER=sbanken
 
 function ssh_command {
     cmd="$@"
@@ -8,10 +8,10 @@ function ssh_command {
 }
 
 ssh_command mkdir -p $DEST_FOLDER
-ssh_command $DEST_FOLDER/bin/sbanken_monitor stop
+ssh_command $DEST_FOLDER/bin/sbanken stop
 ssh_command rm -rf $DEST_FOLDER/*
 scp -q -oStrictHostKeyChecking=no -i $KEY_FILE -r $BUILD $DEPLOY_TARGET:~/ &&
-    ssh_command tar -zxvf ~/sbanken_monitor.tar.gz -C $DEST_FOLDER &&
-    ssh_command LANG=en_US.utf8 $DEST_FOLDER/bin/sbanken_monitor start
+    ssh_command tar -zxvf ~/sbanken.tar.gz -C $DEST_FOLDER &&
+    ssh_command LANG=en_US.utf8 $DEST_FOLDER/bin/sbanken start
 
 
